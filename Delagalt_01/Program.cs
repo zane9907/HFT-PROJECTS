9c57dc82713e0115e765e376367c3da14ffb2fb0
Botond Antalóczy <zane9907@gmail.com>
2022-08-13 14:35:31 +0200

    First task done

--- Delagalt_01/Program.cs
+++ Delagalt_01/Program.cs
@@ -2,39 +2,92 @@
 
 namespace Delagalt_01
 {
-    delegate int Muveletek(int x, int y);
+    delegate void Muvelet(int a, int b);
+
+    class Szamok
+    {
+        public event Muvelet muvelet;
+
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public Szamok()
+        {
+            Random rnd = new Random();
+            A = rnd.Next(1, 10);
+            B = rnd.Next(1, 10);
+        }
+
+        public void Szamol(Muvelet m)
+        {
+            m?.Invoke(A, B);
+        }
+
+        public void Szamol()
+        {
+            muvelet?.Invoke(A, B);
+        }
+    }
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            Muveletek m = new Muveletek(Osszeadas);
+            Muvelet m = new Muvelet(Osszeadas);
             m += Kivonas;
             m += Szorzas;
             m += Osztas;
 
+            //Console.WriteLine("Eredmény: " + m(5, 2)); Console.WriteLine();
 
-            
+
+            //foreach (var item in m.GetInvocationList())
+            //{
+            //    Console.WriteLine((item as Muvelet)?.Invoke(5, 2));
+            //}
+
+
+            Szamok sz = new Szamok();
+            Console.WriteLine($"A: {sz.A}\nB: {sz.B}");
+
+            Console.WriteLine("Delegált:");
+            sz.Szamol(m);
+
+
+            Console.WriteLine("\nEsemény:");
+            sz.muvelet += m;
+            sz.Szamol();
+
+            sz = null;
+
+            //m -= Osszeadas;
+            //m -= Kivonas;
+            //m -= Szorzas;
+            //m -= Osztas;
+
+            ;
+
         }
 
-        static int Osszeadas(int x, int y)
+        static void Osszeadas(int x, int y)
         {
-            return x + y;
+            Console.WriteLine($"Összeadás: {x + y}");
         }
 
-        static int Kivonas(int x, int y)
+        static void Kivonas(int x, int y)
         {
-            return x - y;
+            Console.WriteLine($"Kivonás: {x - y}");
         }
 
-        static int Szorzas(int x, int y)
+        static void Szorzas(int x, int y)
         {
-            return x * y;
+            Console.WriteLine($"Szorzás: {x * y}");
         }
 
-        static int Osztas(int x, int y)
+        static void Osztas(int x, int y)
         {
-            return x / y;
+            Console.WriteLine($"Osztás: {x / y}");
         }
     }
 }
