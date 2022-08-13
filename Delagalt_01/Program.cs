82513c046c9b390140ac0ad74bedb2b92b1b9d68
Botond Antalóczy <zane9907@gmail.com>
2022-08-13 12:44:50 +0200

    Add project files.

---
+++ Delagalt_01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Delagalt_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
