abfdbf294653260643e8f4eebacf898b5b282573
Botond Antalóczy <zane9907@gmail.com>
2022-08-15 22:17:34 +0200

    Created Json file

--- XML_JSON_LINQ_02/Program.cs
+++ XML_JSON_LINQ_02/Program.cs
@@ -1,12 +1,61 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
 
 namespace XML_JSON_LINQ_02
 {
-    internal class Program
+    class CD
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Country { get; set; }
+        public string Company { get; set; }
+        public double Price { get; set; }
+        public int Year { get; set; }
+
+        public CD(string title, string artist, string country, string company, double price, int year)
+        {
+            Title = title;
+            Artist = artist;
+            Country = country;
+            Company = company;
+            Price = price;
+            Year = year;
+        }
+
+        public CD()
+        {
+
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<CD> catalog = new List<CD>();
+
+            XDocument xdoc = XDocument.Load("cd_catalog.xml");
+
+            foreach (var item in xdoc.Root.Elements())
+            {
+                catalog.Add(new CD()
+                {
+                    Title = item.Element("TITLE").Value,
+                    Artist = item.Element("ARTIST").Value,
+                    Country = item.Element("COUNTRY").Value,
+                    Company = item.Element("COMPANY").Value,
+                    Price = double.Parse(item.Element("PRICE").Value),
+                    Year = int.Parse(item.Element("YEAR").Value)
+                });
+            }
+
+            
+            ;
+
         }
     }
 }
