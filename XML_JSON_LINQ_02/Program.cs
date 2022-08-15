e13968e299e8483027bce38e0f439aaa7612633e
Botond Antalóczy <zane9907@gmail.com>
2022-08-15 20:05:40 +0200

    changes

---
+++ XML_JSON_LINQ_02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML_JSON_LINQ_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
