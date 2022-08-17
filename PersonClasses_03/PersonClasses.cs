2ef0071a21518d0da46c83c84ebb37ec1d1a6e9e
Botond Antalóczy <zane9907@gmail.com>
2022-08-17 23:16:58 +0200

    done

--- PersonClasses_03/PersonClasses.cs
+++ PersonClasses_03/PersonClasses.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
 
 namespace PersonClasses_03
 {
@@ -9,6 +12,7 @@ namespace PersonClasses_03
 
     public abstract class Person : IPerson
     {
+        [NameChecker(5)]
         public string Name { get; set; }
         public int Age { get; set; }
 
@@ -17,39 +21,90 @@ namespace PersonClasses_03
             return $"Name: {Name} - Age: {Age}";
         }
 
-        public abstract void Do();
+        protected Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public abstract string Description();
     }
 
     public class Worker : Person
     {
+        public Worker(string name, int age) : base(name, age)
+        {
+        }
+
         public string Job { get; set; }
         public int Salary { get; set; }
 
-        public override void Do()
+        public override string Description()
         {
-            Console.WriteLine($"Working as {Job} for {Salary}$/hour");
+            return $"Working hard for the minimal wage.";
         }
     }
 
     public class Student : Person
     {
+        public Student(string name, int age) : base(name, age)
+        {
+        }
+
         public string NeptunID { get; set; }
         public string Email { get; set; }
         public int Credits { get; set; }
 
-        public override void Do()
+        public override string Description()
         {
-            Console.WriteLine($"[{NeptunID}] Need more credits.");
+            return $"Always in need for credits.";
         }
     }
 
     public class Retired : Person
     {
+        public Retired(string name, int age) : base(name, age)
+        {
+        }
+
         public int SulejmanEpisodes { get; set; }
 
-        public override void Do()
+        public override string Description()
         {
-            Console.WriteLine("Watching Szulejmán all day.");
+            return "Watching Szulejmán all day.";
+        }
+    }
+
+    public static class ClassDetector
+    {
+        public static void ClassesToXML()
+        {
+            Assembly a = Assembly.GetExecutingAssembly();
+
+            Type[] types = a.GetTypes().Where(x => x.IsClass && x.GetInterface("IPerson") != null).OrderBy(x => x.Name).ToArray();
+
+            XDocument xdoc = new XDocument();
+            xdoc.Add(new XElement("people", new XAttribute("count", types.Length)));
+            foreach (var t in types)
+            {
+                xdoc.Root.Add(new XElement("class",
+                    new XElement("name", t.Name),
+                    new XElement("hash", t.GetHashCode())));
+            }
+
+            xdoc.Save("classes.xml");
+            ;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class NameCheckerAttribute : Attribute
+    {
+        public int Length { get; set; }
+
+        public NameCheckerAttribute(int length)
+        {
+            Length = length;
         }
     }
 }
--- Reflection_03/Program.cs
+++ Reflection_03/Program.cs
@@ -1,12 +1,116 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using PersonClasses_03;
 
 namespace Reflection_03
-{
+{ 
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Random rnd = new Random();
+
+            List<IPerson> people = new List<IPerson>();
+            Func<Type> typeRandomizer = () =>
+            {
+                int num = rnd.Next(3);
+                switch (num)
+                {
+                    case 0: return typeof(Worker);
+                    case 1: return typeof(Student);
+                    default: return typeof(Retired);
+                }
+            };
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                string rName = "";
+                for (int j = 0; j < rnd.Next(3, 11); j++)
+                    rName += ((char)rnd.Next('A', 'Z')).ToString();
+
+                int rAge = rnd.Next(1, 101);
+
+
+                IPerson p = (IPerson)Activator.CreateInstance(typeRandomizer(), new object[] { rName, rAge });
+                people.Add(p);
+                Console.WriteLine($"Added new {p.GetType().Name}: {p}");
+            }
+            Console.WriteLine();
+
+            foreach (var item in people)
+            {
+                Type t = item.GetType();
+
+                Console.WriteLine("\n");
+                Console.WriteLine("TYPE: " + t.Name);
+
+                Console.WriteLine("FIELDS: ");
+                foreach (var f in t.GetFields())
+                {
+                    Console.WriteLine("-> " + f.Name);
+                }
+
+                Console.WriteLine("PROPERTIES: ");
+                foreach (var p in t.GetProperties())
+                {
+                    Console.WriteLine("-> " + p.Name);
+                }
+
+                Console.WriteLine("METHODS: ");
+                foreach (var m in t.GetMethods())
+                {
+                    Console.WriteLine("-> " + m.Name);
+                }
+            }
+
+            Console.WriteLine("\n\n");
+            foreach (var p in people)
+            {
+                Type t = p.GetType();
+
+                PropertyInfo info = t.GetProperty("Name");
+                MethodInfo m = t.GetMethod("Description");
+
+
+                string result = info.GetValue(p) + ": " + (string)m?.Invoke(p, null);
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("\n\n");
+
+            foreach (var p in people)
+            {
+                NameValidator(p);
+            }
+
+
+            ClassDetector.ClassesToXML();
+
+            ;
+        }
+
+
+
+        static void NameValidator(object o)
+        {
+            PropertyInfo info = o.GetType().GetProperty("Name");
+            NameCheckerAttribute attr = (NameCheckerAttribute)info.GetCustomAttribute(typeof(NameCheckerAttribute));
+
+            if (attr != null)
+            {
+                if (attr.Length <= info.GetValue(o).ToString().Length)
+                {
+                    Console.WriteLine($"Name accpeted. ({info.GetValue(o)})");
+                }
+                else
+                {
+                    Console.WriteLine($"Name is too short. ({info.GetValue(o)})");
+                }
+            }
         }
     }
 }
